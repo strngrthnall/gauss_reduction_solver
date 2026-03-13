89bbdf21d2e6e6b1f5f711888f73db5f3b302a80
mvrmiranda <marcos.miranda.ext@outlook.com>
2026-03-13 18:09:44 -0300

    Refatorar implementação da eliminação gaussiana e adicionar função de arredondamento

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
-#[derive(Debug)]
-struct System {
-    matrix: [[f64; 4]; 3],
+fn rounder(mut num: f64) -> f64 {
+    let epsilon = 1e-10;
+
+    let rounded = num.round();
+
+    if (num - rounded).abs() < epsilon {
+        num = rounded;
+    }
+
+    if num.abs() < epsilon {
+        num = 0.0;
+    }
+
+    num
 }
 
-fn gaussian_elimination(system: &mut System) -> [[f64; 4]; 3] {
-    let matrix = &mut system.matrix;
-
+fn row_reduction(matrix: &mut [[f64; 4]; 3]) -> [[f64; 4]; 3] {
     let n = matrix.len();
     
     for k in 0..n {
 
-        if matrix[k][k] == 0.0 && k < n-2 {
-            let buffer = matrix[k];
+        let mut max_row = k;
 
-            matrix[k] = matrix[k+1];
-            matrix[k+1] = buffer;
+        for i in k+1..n {
+            if matrix[i][k].abs() > matrix[max_row][k].abs() {
+                max_row = i
+            }
+        }
+
+        if max_row != k {
+            matrix.swap(k, max_row);
         }
 
         for i in k+1..n {
@@ -27,25 +41,121 @@ fn gaussian_elimination(system: &mut System) -> [[f64; 4]; 3] {
 
     }
 
-    let new_matrix = matrix.clone();
+    for i in 0..n {
+        for j in 0..=n {
+            matrix[i][j] = rounder(matrix[i][j])
+        }
+    }
 
-    new_matrix
+    *matrix
+}
 
+fn solver(matrix: [[f64; 4]; 3]) -> Result<[f64; 3], &'static str> {
+
+    let mut solutions = [0.0; 3];
+
+    let n = matrix.len();
+
+    let epsilon = 1e-10;
+
+    for i in (0..n).rev() {
+        let mut acc = matrix[i][n];
+
+        for j in i+1..n {
+            acc -= matrix[i][j] * solutions[j];
+        }
+
+        if matrix[i][i].abs() < epsilon {
+            return Err("Sistema Singular: O sistema é Impossível (SI) ou Indeterminado (SPI)")
+        }
+
+        solutions[i] = acc / matrix[i][i];
+    }
+
+    for i in 0..n {
+        solutions[i] = rounder(solutions[i])
+    }
+
+    Ok(solutions)
 }
 
 fn main() {    
-    let mut system = System { 
-        matrix: [
-            [20.0, 40.0, 20.0, 1300.0], 
-            [20.0, 10.0, 30.0, 1100.0], 
-            [24.0, 24.0, 16.0, 1000.0]
-        ]
-    };
 
-    let new_matrix = gaussian_elimination(&mut system);
+    let mut matrix_si: [[f64; 4]; 3] = [
+        [1.0, 1.0, 1.0, 1.0],
+        [2.0, 2.0, 2.0, 5.0],
+        [1.0, -1.0, 2.0, 3.0]
+    ];
+
+    let new_matrix = row_reduction(&mut matrix_si);
     
     for line in new_matrix {
         println!("{:?}", line)
     }
 
+    match solver(new_matrix) {
+        Ok(solution) => println!("Soluções - {:?}", solution),
+        Err(e) => println!("Falha ao resolver: {}", e)
+    }
+
 }
+
+#[test]
+fn reduction_on_simple_matrix() {
+    let mut matrix = [
+        [10.0, 12.0, 15.0, 960.0], 
+        [6.0, 8.0, 12.0, 660.0], 
+        [12.0, 12.0, 18.0, 1080.0]
+    ];
+
+    let answer = [
+        [12.0, 12.0, 18.0, 1080.0],
+        [0.0, 2.0, 3.0, 120.0],
+        [0.0, 0.0, -3.0, -60.0]
+    ];
+
+    assert_eq!(row_reduction(&mut matrix), answer)
+}
+
+#[test]
+fn solution_on_simple_matrix() {
+    let mut matrix = [
+        [1.0, 1.0, 1.0, 6.0],
+        [-10.0, 2.0, -1.0, -9.0],
+        [3.0, -2.0, 1.0, 2.0]
+    ];
+
+    let reduction = row_reduction(&mut matrix);
+    let expected = [1.0, 2.0, 3.0];
+
+    let solution = solver(reduction).unwrap();
+    assert_eq!(solution, expected);
+
+}
+
+#[test]
+fn impossible_system() {
+    let mut matrix: [[f64; 4]; 3] = [
+        [1.0, 1.0, 1.0, 1.0],
+        [2.0, 2.0, 2.0, 5.0],
+        [1.0, -1.0, 2.0, 3.0]
+    ];
+
+    let new_matrix = row_reduction(&mut matrix);
+
+    assert!(solver(new_matrix).is_err())
+}
+
+#[test]
+fn underteminated_system() {
+    let mut matrix: [[f64; 4]; 3] = [
+    [1.0, 1.0, 1.0, 2.0],
+    [2.0, -1.0, 1.0, 3.0],
+    [3.0, 0.0, 2.0, 5.0]
+];
+
+    let new_matrix = row_reduction(&mut matrix);
+
+    assert!(solver(new_matrix).is_err())
+}
+
